f822c4d7379528e8066603265ecbc18b395271a8
unknown <1251658835@qq.com>
2021-04-02 20:20:24 +0800

    随便整了个follow集

--- Grammer.py
+++ Grammer.py
@@ -22,8 +22,14 @@ first_sets = dict()
 follow_sets = dict()
 predict_sets = dict()
 
+def sets_init():
+    for symbol in NT:
+            first_sets[symbol] = set()
+            follow_sets[symbol] = set()
+            predict_sets[symbol] = set()
 
-
+    follow_sets['Program'].add('#')
+sets_init()
 f = open("normalGrammer.txt","r").readlines()
 for grammer in f:
     grammer = grammer.strip("\n").split("::=")
@@ -32,7 +38,8 @@ for grammer in f:
     productions[left] = right
 
 
-# print(productions)
+# for production in productions:
+#     print(production+"\t",productions[production])
 
 #用于获得产生式右部第一个符号
 def getFirstSymbol(right_expr:str):
@@ -43,7 +50,6 @@ def getFirstSymbol(right_expr:str):
 
 def getFirstSet(symbol:str):
     right_exprs = productions[symbol]
-    l = len(right)
     first_count = 0
     first = set()
     for right_expr in right_exprs:
@@ -56,14 +62,68 @@ def getFirstSet(symbol:str):
         first_count += 1
     return first
 
-f = open("FirstSet.txt","w")
 
 for i in NT:
     sample_first = getFirstSet(i)
-    f.write(i+"\t"+str(sample_first)+"\n")
+    first_sets[i] = sample_first
 
-f.close()
-        
+
+for i in T:
+    first_sets[i] = set(i)
+
+
+# def getFollowSet():
+#     index = 0
+#     next = 'a'
+#     modified = 1
+#     while modified == 1:
+#         print("233")
+#         modified = 0
+#         for symbol in NT:
+#         #遍历所有非终极符
+#             for leftSymbol in NT:
+#                 #此处遍历非终极符目的是为了在遍历所有产生式
+#                 right_exprs = productions[leftSymbol]
+#                 for right_expr in right_exprs:
+#                     symbol_list = right_expr.strip(" ").split(" ")
+#                     #将产生式右部拆分为list，便于寻找下一个元素
+#                     if symbol in symbol_list:
+#                         index = symbol_list.index(symbol)
+#                         preFollow_set = follow_sets[symbol]
+#                         #找到当前需要找follow集的符号的索引
+#                         if index == len(symbol_list) - 1:
+#                             follow_sets[symbol] = follow_sets[symbol].union(follow_sets[leftSymbol])
+
+#                         else:
+#                             next = symbol_list[index+1]
+#                             #其必须拥有下一个元素时才适用此种分析方法
+                            
+#                             if 'ε' not in first_sets[next]:
+#                                 follow_sets[symbol] = first_sets[next]
+#                             else:
+#                                 follow_sets[symbol] = (first_sets[next]-set('ε')).union(follow_sets[leftSymbol])
+#                         if len(follow_sets[symbol].intersection(preFollow_set)) == len(follow_sets[symbol]) == len(preFollow_set):
+#                             modified = 0
+#                         else:
+#                             modified = 1
+
+# getFollowSet()               
+# print(type(first_sets['ε']-set('ε')))
+# f = open("FollowSet.txt","w")
+# for nt in NT:
+#     f.write(nt+"\t"+str(follow_sets[nt])+"\n")
+
+
+
+
+
+
+            
+            
+
+
+
+
 
 
 
